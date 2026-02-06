21994f5365292122f19554c08c966ee3964b9f0f
bobababooon <bobababoon1230@gmail.com>
2026-02-06 13:19:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const massage = document.getElementById("massage")
-const next = document.getElementById("next")
+const massage = document.getElementById("massage");
+const next = document.getElementById("next");
 
-const massage = ["こんにちは","こんばんは"]
+const messages = ["こんにちは", "こんばんは"];
 
 let index = 0;
 
-next.addEventListener("click",() => {
-  index = index+1;
+next.addEventListener("click", () => {
+  index++;
 
-                      if(index >= message.length) {
-                        index=0;
-                      }
-   message.textContent = messages[index];
-})
+  if (index >= messages.length) {
+    index = 0;
+  }
+
+  massage.textContent = messages[index];
+});
